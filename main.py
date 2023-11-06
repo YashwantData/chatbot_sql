7617fc7436dad7b56f4dbdf8671ded3cf9eb535a
YashwantData <raiyashwant987@gmail.com>
2023-11-06 06:13:28 +0530

    define function and adding if conditions as requirements

--- main.py
+++ main.py
@@ -20,73 +20,86 @@ for message in st.session_state["messages"]:
             st.markdown(message["content"])
 
 #Header of the Page
-st.header("Get your Database Info")
+
 
 #Sidebar Content
 with st.sidebar:
+    st.header("Get your Database Info")
     key = st.text_input("Add your API Key")
-    os.environ["OPENAI_API_KEY"] = key
-    print(key)
-    llm = OpenAI(temperature=0)
-    st.markdown('''
-    ## About APP:
+    if key:
+        
+       
+        
+        st.markdown('''
+        ## About APP:
 
-    The app's primary resource is utilised to create::
+        The app's primary resource is utilised to create::
 
-    - [streamlit](https://streamlit.io/)
-    - [Langchain](https://docs.langchain.com/docs/)
-    - [OpenAI](https://openai.com/)
+        - [streamlit](https://streamlit.io/)
+        - [Langchain](https://docs.langchain.com/docs/)
+        - [OpenAI](https://openai.com/)
 
-    ## About me:
+        ## About me:
 
-    - [Linkedin](https://www.linkedin.com/in/yashwant-rai-2157aa28b)
+        - [Linkedin](https://www.linkedin.com/in/yashwant-rai-2157aa28b)
 
-    ''')
+        ''')
 
-    st.write('💡All about SQL Database based chatbot, created by Yashwant Rai')
+        st.write('💡All about SQL Database based chatbot, created by Yashwant Rai')
 
-# Create an SQLAlchemy engine for the MSSQL database
-db_url_1 = os.environ.get('DB_CONNECTION',
-                          'mssql+pyodbc://DESKTOP-SVULHV9\SQLEXPRESS/AdventureWorksLT2022?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server')
-# db_url_2 = os.environ.get('DB_CONNECTION','mssql+pyodbc://DESKTOP-SVULHV9\SQLEXPRESS/AdventureWorksDW2019?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server')
+        
+def main(key):
+    if key:
+        os.environ["OPENAI_API_KEY"] = key
+        
+        llm = OpenAI(temperature=0)
+        # Create an SQLAlchemy engine for the MSSQL database
+        db_url_1 = os.environ.get('DB_CONNECTION','mssql+pyodbc://DESKTOP-SVULHV9\SQLEXPRESS/AdventureWorksLT2022?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server')
+        # db_url_2 = os.environ.get('DB_CONNECTION','mssql+pyodbc://DESKTOP-SVULHV9\SQLEXPRESS/AdventureWorksDW2019?trusted_connection=yes&driver=ODBC+Driver+17+for+SQL+Server')
 
 
-engine = sqlalchemy.create_engine(db_url_1)
+        engine = sqlalchemy.create_engine(db_url_1)
 
-db = SQLDatabase(engine)
+        db = SQLDatabase(engine)
+        
 
-# Create a SQLDatabaseToolkit instance using the SQLDatabase and OpenAI instances
-toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+        # Create a SQLDatabaseToolkit instance using the SQLDatabase and OpenAI instances
+        toolkit = SQLDatabaseToolkit(db=db, llm=llm)
 
-# Now you can use the 'toolkit' to perform SQL-related operations on your MSSQL database
-agent_executor = create_sql_agent(
-    llm=llm,
-    toolkit=toolkit,
-    verbose=True,
-    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-)
+        # Now you can use the 'toolkit' to perform SQL-related operations on your MSSQL database
+        agent_executor = create_sql_agent(
+            llm=llm,
+            toolkit=toolkit,
+            verbose=True,
+            agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        )
 
-# Accept user questions/query
-query = st.chat_input(placeholder="Ask questions about your Database:")
+        # Accept user questions/query
+        query = st.chat_input(placeholder="Ask questions about your Database:")
 
-if query:
-    chat_history = []
-    with st.chat_message("user"):
-        st.markdown(query)
-    st.session_state.messages.append({"role": "user", "content": query})
-    custom_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question. At the end of standalone question add this 'Answer the question in English language.' If you do not know the answer reply with 'I am sorry'.
-                            Chat History:
-                            {chat_history}
-                            Follow Up Input: {question}
-                            Standalone question:
-                            Remember to greet the user with hi welcome to pdf chatbot how can i help you? if user asks hi or hello """
+        if query:
+            chat_history = []
+            with st.chat_message("user"):
+                st.markdown(query)
+            st.session_state.messages.append({"role": "user", "content": query})
+            custom_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question. At the end of standalone question add this 'Answer the question in English language.' If you do not know the answer reply with 'I am sorry'.
+                                    Chat History:
+                                    {chat_history}
+                                    Follow Up Input: {question}
+                                    Standalone question:
+                                    Remember to greet the user with hi welcome to pdf chatbot how can i help you? if user asks hi or hello """
 
-    CUSTOM_QUESTION_PROMPT = PromptTemplate.from_template(custom_template)
+            CUSTOM_QUESTION_PROMPT = PromptTemplate.from_template(custom_template)
 
-    response = agent_executor.run(query)
+            response = agent_executor.run(query)
 
-    # st.write(response["answer"])
-    with st.chat_message("assistant"):
-        st.markdown(response)
-    st.session_state.messages.append({"role": "assistant", "content": response})
-    chat_history.append((query, response))
+            # st.write(response["answer"])
+            with st.chat_message("assistant"):
+                st.markdown(response)
+            st.session_state.messages.append({"role": "assistant", "content": response})
+            chat_history.append((query, response))
+
+if __name__ == '__main__':
+    main(key)
+
+
